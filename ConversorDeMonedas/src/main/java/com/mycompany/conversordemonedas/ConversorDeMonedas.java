20caccc5c93c7560bfc2f0ec9b5e8977c2442505
AlxEdwuin <93042290+AlxEdwuin@users.noreply.github.com>
2023-03-01 20:00:50 -0600

    Add files via upload

---
+++ ConversorDeMonedas/src/main/java/com/mycompany/conversordemonedas/ConversorDeMonedas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.conversordemonedas;
+
+/**
+ *
+ * @author Alexander
+ */
+public class ConversorDeMonedas {
+    
+    
+
+    public static void main(String[] args) {
+       Inicio inicio = new Inicio();
+       inicio.setVisible(true);
+    }
+}
